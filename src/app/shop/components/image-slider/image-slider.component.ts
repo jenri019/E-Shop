90363f99ce3d85197044eb5ca7b5b9d4793f69f1
jenri019 <j.enrique209436@hotmail.com>
2021-12-14 18:08:34 -0600

    Adding images and styles for the slider

--- src/app/shop/components/image-slider/image-slider.component.ts
+++ src/app/shop/components/image-slider/image-slider.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageSliderComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   public _imgURL:String[] = [
+    'https://images.pexels.com/photos/10013089/pexels-photo-10013089.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/6405575/pexels-photo-6405575.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/8567136/pexels-photo-8567136.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/10013089/pexels-photo-10013089.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/6405575/pexels-photo-6405575.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/8567136/pexels-photo-8567136.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/10013089/pexels-photo-10013089.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/6405575/pexels-photo-6405575.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/8567136/pexels-photo-8567136.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/10013089/pexels-photo-10013089.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/6405575/pexels-photo-6405575.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/8567136/pexels-photo-8567136.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
     'https://images.pexels.com/photos/10013089/pexels-photo-10013089.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
     'https://images.pexels.com/photos/6405575/pexels-photo-6405575.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
     'https://images.pexels.com/photos/8567136/pexels-photo-8567136.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
@@ -20,6 +33,31 @@ export class ImageSliderComponent implements OnInit {
 }
 
   ngOnInit(): void {
+  }
+
+  left() {
+    let slider = document.getElementById("slider-container");
+    let left = <number>slider?.scrollLeft;
+    let width = <number>slider?.scrollWidth;
+    
+    if(left === 0){
+      slider?.scrollTo(width,0);
+    }else{
+      slider?.scrollTo(left-100, 0);
+    }
+  }
+
+  right() {
+    let slider = document.getElementById("slider-container");
+    let right = <number>slider?.scrollLeft;
+    let div = <number>slider?.offsetWidth;
+    let width = <number>slider?.scrollWidth;
+    
+    if(right < (width-div)){
+      slider?.scrollTo(right+100, 0);
+    }else{
+      slider?.scrollTo(0, 0);
+    }
   }
 
 }
