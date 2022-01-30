8f6d28310e3d6b3a0416c1736e7190ac861a11de
jenri019 <j.enrique209436@hotmail.com>
2022-01-29 21:16:06 -0600

    Verifying route and saving index to service

--- src/app/shop/pages/product/product.component.ts
+++ src/app/shop/pages/product/product.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../../models/product.interface';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent {
+  public productList:Product[] = this.productService.products;
+  public pid:string = '';
+  public existence:boolean = false;
 
-  constructor() { }
+  constructor(
+    private productService:ProductService,
+    private route:ActivatedRoute) {
+      this.pid = <string>this.route.snapshot.paramMap.get('productID');
+      for(let i = 0; i < this.productList.length; i++){
+        if(this.pid == this.productList[i].id){
+          this.existence = true;
+          this.productService.setIndice(i);
+        }
+      }
+    }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
