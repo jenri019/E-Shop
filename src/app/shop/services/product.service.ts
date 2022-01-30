c9fea986ade7cfe55cecef44419e1f374f2ce28e
jenri019 <j.enrique209436@hotmail.com>
2022-01-29 21:14:07 -0600

    Changing types and adding values in service

--- src/app/shop/services/product.service.ts
+++ src/app/shop/services/product.service.ts
@@ -5,26 +5,72 @@ import { Product } from "src/app/models/product.interface";
 
 export class ProductService {
 
+    private _index:number = 0;
     private _products:Product[] = [
         {
-            id:1223,
+            id:"1223",
             name: "Televisor",
             price: 2500,
             inOffer: false,
             stock: 23,
-            description: "¨This is a product",
+            description: "¨This is a TV",
             features: [
-                {'color':'prasd'},
-                {'color':'prasd'},
-                {'color':'prasd'},
-                {'color':'prasd'},
-                {'color':'prasd'},
+                {
+                    color:'azul',
+                    peso:'poco',
+                    marca:'juan',
+                    año:'2020',
+                }
             ]
-        }
+        },
+        {
+            id:"1233",
+            name: "Computadora",
+            price: 2500,
+            inOffer: false,
+            stock: 23,
+            description: "¨This is a computer",
+            features: [
+                {
+                    color:'verde',
+                    peso:'11 kg',
+                    marca:'chapulin',
+                    año:'1996',
+                }
+            ]
+        },
+        {
+            id:"1243",
+            name: "Radio",
+            price: 2500,
+            inOffer: false,
+            stock: 23,
+            description: "¨This is a Radio",
+            features: [
+                {
+                    color:'rojo',
+                    peso:'mucho',
+                    marca:'superpato',
+                    año:'2021',
+                    estado:'decente',
+                    frecuencia:'alta',
+                    longitud:'alta',
+                    procesador:'apoco lleva?',
+                }
+            ]
+        },
     ];
 
     get products():Product[] {
         return [...this._products];
+    }
+
+    get index():number{
+        return this._index;
+    }
+
+    setIndice(index:number){
+        this._index = index;
     }
 
     constructor(){}
