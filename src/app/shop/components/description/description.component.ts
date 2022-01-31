0cef50830c9947f78824cb3b7e9c3ff4eb232e57
jenri019 <j.enrique209436@hotmail.com>
2022-01-30 22:48:08 -0600

    Showing info based on the selected option

--- src/app/shop/components/description/description.component.ts
+++ src/app/shop/components/description/description.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product.interface';
 import { ProductService } from '../../services/product.service';
+import { Element } from '@angular/compiler';
 
 @Component({
   selector: 'app-description',
@@ -13,8 +14,10 @@ export class DescriptionComponent implements OnInit {
   public keys:string[] = [];
   public values:string[] = [];
   public description = "";
-  
 
+
+  public display:boolean = true;
+  
   constructor(private productService:ProductService)
   {
     this.description = this.product[0].description;
@@ -29,4 +32,32 @@ export class DescriptionComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  displayDescription() {
+    if(!this.display){
+      document.getElementById('description-title')?.classList.remove('non-display');
+      document.getElementById('features-title')?.classList.remove('display');
+      document.getElementById('description')?.classList.remove('non-display');
+      document.getElementById('features')?.classList.remove('display');
+      document.getElementById('description-title')?.classList.add('display');
+      document.getElementById('features-title')?.classList.add('non-display');
+      document.getElementById('description')?.classList.add('display');
+      document.getElementById('features')?.classList.add('non-display');
+      this.display = true;
+    }
+  }
+
+  displayFeatures() {
+    if(this.display){
+      document.getElementById('description-title')?.classList.remove('display');
+      document.getElementById('features-title')?.classList.remove('non-display');
+      document.getElementById('description')?.classList.remove('display');
+      document.getElementById('features')?.classList.remove('non-display');
+      document.getElementById('description-title')?.classList.add('non-display');
+      document.getElementById('features-title')?.classList.add('display');
+      document.getElementById('description')?.classList.add('non-display');
+      document.getElementById('features')?.classList.add('display');
+      this.display = false;
+    }
+  }
 }
