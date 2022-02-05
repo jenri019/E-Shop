0e138b7716aed8a72710205fbf4aa5fef4f1530e
jenri019 <j.enrique209436@hotmail.com>
2022-02-05 13:44:05 -0600

    Showing service info in main

--- src/app/shop/components/card-product/card-product.component.ts
+++ src/app/shop/components/card-product/card-product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-product',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardProductComponent implements OnInit {
 
-  constructor() { }
+  @Input() name:string = "";
+  @Input() price:number = 0;
+  @Input() id:string = "";
+
+  constructor() {}
 
   ngOnInit(): void {
   }
